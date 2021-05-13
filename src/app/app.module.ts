f4011bf6e474aa39bb0e87b0955c63153cc1a364
carmit vaknin <carmit.vk@gmail.com>
2021-05-13 17:16:56 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './cmps/header/header.component';
+import { ClockComponent } from './cmps/clock/clock.component';
+import { DateCmpComponent } from './cmps/date-cmp/date-cmp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    ClockComponent,
+    DateCmpComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
